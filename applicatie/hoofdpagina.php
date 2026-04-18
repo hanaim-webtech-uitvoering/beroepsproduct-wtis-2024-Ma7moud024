69e014ef2f33c0fb84055099280711801b74365d
Ma7moud024 <M.ZindAlHadid@student.han.nl>
2026-04-18 15:00:33 +0200

    Begin met hoofdpagina

---
+++ applicatie/hoofdpagina.php
@@ -0,0 +1,40 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hoofdpagina</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <a href="menu.php"><button>Menu</button></a>
+            <a href="profiel.php"><button>Profiel</button></a>
+            <a href="registratie_login.php"><button>Inloggen</button></a>
+
+            <form action="../loguit.php" method="post">
+                <button type="submit">Uitloggen</button>
+            </form>
+
+            <a href="winkelmandje.php"><button>Winkelwagen</button></a>
+            <a href="mijnBestellingen.php"><button>Mijn bestelling</button></a>
+            <a href="bestellingoverzicht_personeel.php"><button>Personeel Bestellingen Overzicht</button></a>
+        </nav>
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+
+    </footer>
+</body>
+
+</html>
